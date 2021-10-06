288e5591acf0bed41a8dc9d8a2da5ce6a74274bc
ukhan64 <92057998+ukhan64@users.noreply.github.com>
2021-10-06 17:13:44 -0400

    third commit

---
+++ src/main/java/testngpractise/Testng1.java
@@ -0,0 +1,13 @@
+package testngpractise;
+
+public class Testng1 {
+
+	public static void main(String[] args) {
+			name();
+
+	}
+
+	public static void name () {System.out.println("my name is UK");}
+	
+	public static void name2 () {System.out.println("my name is king");}
+}
